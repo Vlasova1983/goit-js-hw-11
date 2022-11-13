7668524afac7d1ac1c8175ae734aeb5a0eeef262
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-11-13 17:53:32 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import './css/styles.css';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 
 
 const axios = require('axios');
 const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
+  captionsData: 'alt',
+  captionDelay: 250,
 });
 const btnLoadMore = document.querySelector('.load-more');
 const btnSearch = document.querySelector('[type="submit"]');
@@ -22,7 +23,7 @@ gallery.addEventListener('click', onGalleryClick);
 function onClickBtnSearch(evt) {
   evt.preventDefault();
   fetchImages(input.value.trim());
-  btnLoadMore.classList.remove('clear');
+ 
 }
 
 function onGalleryClick(evt) {
@@ -30,39 +31,40 @@ function onGalleryClick(evt) {
 }
 
 function fetchImages(name){    
-    axios.get('/user?ID=12345')
-    .then(response=>{
-      return response.json();
+  fetch(`https://pixabay.com/api/?key=31294159-be9d27b57dbd5b4db758a00af&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`)
+  .then(response=>{
+    return response.json();
     })
-    .then(image=>{
-        if (image.length===0){            
-          Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
-        } 
-        else { // largeImageURL - ссылка на большое изображение.            
-            gallery.innerHTML = image.map(item=>
-                `<div class="photo-card">
-                <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-                <div class="info">
-                  <p class="info-item">
-                    <b>Likes</b>${likes}
-                  </p>
-                  <p class="info-item">${views}
-                    <b>Views</b>
-                  </p>
-                  <p class="info-item">
-                    <b>Comments</b>${comments}
-                  </p>
-                  <p class="info-item">
-                    <b>Downloads</b>${downloads}
-                  </p>
-                </div>
-              </div> `) 
-            .join('');                   
-        }         
-    })
-    .catch(error=>{
-        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-    })
+  .then(image=>{
+    if (image.hits.length === 0){  
+      Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    } 
+    else { // largeImageURL - ссылка на большое изображение.  
+      btnLoadMore.classList.remove('clear');          
+      gallery.innerHTML = image.hits.map(item=>
+        `<div class="photo-card">
+          <img src="${item.webformatURL}" alt="${item.tags}" loading = "lazy" width=120/>
+            <div class="info">
+              <p class="info-item">
+                <b>Likes</b>${item.likes}
+              </p>
+              <p class="info-item">
+                <b>Views</b>${item.views}
+              </p>
+              <p class="info-item">
+                <b>Comments</b>${item.comments}
+              </p>
+              <p class="info-item">
+                <b>Downloads</b>${item.downloads}
+              </p>
+            </div>
+        </div> `) 
+      .join('');                   
+    }         
+  })
+  .catch(error=>{
+     Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+  })
 } 
 
 
